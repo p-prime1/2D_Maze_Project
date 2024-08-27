ee39a42a838888a7292f01968466615bd82a004a
p-prime1 <preciousnwaka95@gmail.com>
2024-08-27 13:15:03 +0100

    Added the render function prototype

--- main.h
+++ main.h
@@ -15,18 +15,21 @@
 /**
  * struct Resource - Struct to hold SDL resources
  * @window: Window to be rendered to
- * @renderer: Rendered to
+ * @renderer: Window renderer
  * @surface: Surface to be rendered to
+ * @Texture: Displayed texture
  */
 
 typedef struct Resource
 {
 	SDL_Window *window;
-	SDL_Window *renderer;
+	SDL_Renderer *renderer;
 	SDL_Surface *surface;
+	SDL_Texture *texture;
 } SDL_Resources;
 
 void free_resource(SDL_Resources *res);
 int sdl_init(SDL_Resources *resources);
+void sdl_renderer(SDL_Resources *res);
 
 #endif
